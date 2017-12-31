190c8257f72074925f9808f3f0f6b701bfeca6bb
RyomaK <ryooomaaa0413@outlook.com>
2017-12-31 19:11:28 +0900

    add tcp client

---
+++ chapter2/tcp-client/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "net"
+
+func main() {
+	target_host := "www.google.com"
+	target_port := "80"
+
+	fmt.Printf("acccess %v:%v", target_host, target_port)
+
+	client, err := net.Dial("tcp", target_host+":"+target_port)
+	if err != nil {
+		panic(err)
+	}
+	sendMsg := "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+	fmt.Fprintf(client, sendMsg)
+	buf := make([]byte, 4096)
+	n, err := client.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Print(string(buf[:n]))
+
+}
